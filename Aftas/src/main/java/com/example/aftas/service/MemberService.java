94f0117937eff6b3580084db7e197397da7802b8
Salah <salaheddinehanaoui@gmail.com>
2023-12-14 16:09:22 +0100

    finished members crud

--- Aftas/src/main/java/com/example/aftas/service/MemberService.java
+++ Aftas/src/main/java/com/example/aftas/service/MemberService.java
@@ -1,4 +1,14 @@
 package com.example.aftas.service;
 
+import com.example.aftas.model.Member;
+
+import java.util.List;
+
 public interface MemberService {
+    Member getMemberById(Long id);
+    List<Member> getAllMembers();
+    Member addMember(Member member);
+    List<Member> findByMembershipNumberOrIdentityNumberOrNameOrFamilyName(String searchTerm);
+    Member updateMember(Member member, Long id);
+    void deleteMember(Long id);
 }
